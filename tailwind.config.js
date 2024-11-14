c5d6382901bd037bddc4c5c466f1c331a1883617
mariselaor <64675015+mariselaor@users.noreply.github.com>
2024-11-14 16:24:14 -0600

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
- 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [daisyui]
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["dark"]
+  }
 }
